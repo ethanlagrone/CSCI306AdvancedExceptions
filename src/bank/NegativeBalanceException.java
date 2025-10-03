cd26a74c2e9587c08b45623c1933d21d59614a8e
ethanlagrone <ethan_lagrone@mines.edu>
2025-10-03 13:45:04 -0600

    Version 1 of NegativeBalanceException, tested with a main, seems to work

--- src/bank/NegativeBalanceException.java
+++ src/bank/NegativeBalanceException.java
@@ -1,24 +1,36 @@
 package bank;
 
-public class NegativeBalanceException {
-	double negativeBalace = 0.0;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class NegativeBalanceException extends Exception {
+	double negativeBalance = 0.0;
 
 	public NegativeBalanceException() {
-		//stub
-		super();
+		super("Error: Negative Balance");
 	}
 
-	public NegativeBalanceException(double negativeBalace) {
-		//use printwriter according to rubric
-		super();
-		this.negativeBalace = negativeBalace;
+	public NegativeBalanceException(double negativeBalance) throws FileNotFoundException {
+		super("Amount exceeds balnce by: " + negativeBalance);
+		this.negativeBalance = negativeBalance;
+		PrintWriter out = new PrintWriter("log.txt");
+		out.write("Amount exceeds balnce by: " + negativeBalance);
+		out.close();
 	}
 
 	@Override
 	public String toString() {
 		//stub generated
-		return "NegativeBalanceException [negativeBalace=" + negativeBalace + "]";
+		return "Balance of " + negativeBalance + " not allowed.";
 	}
 	
+	public static void main(String[] args) throws NegativeBalanceException, FileNotFoundException {
+		double balance = -1.0;
+		if(balance < 0.0) {
+			throw new NegativeBalanceException();
+		}
+	}
+
+
 	
 }
